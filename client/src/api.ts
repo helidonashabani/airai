f768ec9a704b226532734cf16c00de7e56273614
helidona <helidona.shabani@gryn.com>
2023-04-18 11:16:05 +0200

    modified

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosInstance } from 'axios';
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: 'https://main--main-server-airiai.netlify.app/api',
+  baseURL: 'https://main-server-airiai.netlify.app/api',
 });
 
 export default apiClient;
+
